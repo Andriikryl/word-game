fda578642a26dce61f4ce3687942d502809e31fe
Amdrii <andreyk557@gmail.com>
2023-03-19 00:34:40 +0200

    feat[HangmanDrawing]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
-
+import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 import "./App.css";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test");
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="Score">Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
